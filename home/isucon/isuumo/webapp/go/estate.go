544c63a88975b421f8a7063b0ae3bfefc8bac155
bgpat <bgpat23@gmail.com>
2020-09-12 16:30:12 +0900

    Do not use estate_location table

--- home/isucon/isuumo/webapp/go/estate.go
+++ home/isucon/isuumo/webapp/go/estate.go
@@ -9,7 +9,7 @@ import (
 func listEstatesInPolygon(c echo.Context, coordinates Coordinates) ([]Estate, error) {
 	estates := []Estate{}
 	query := fmt.Sprintf(
-		`SELECT estate.* FROM estate JOIN estate_location ON estate.id = estate_location.id WHERE ST_Contains(ST_PolygonFromText(%s), estate_location.location) ORDER BY estate.popularity DESC, estate.id ASC`,
+		`SELECT estate.* FROM estate WHERE ST_Contains(ST_PolygonFromText(%s), POINT(longitude, latitude)) ORDER BY estate.popularity DESC, estate.id ASC`,
 		coordinates.coordinatesToText2(),
 	)
 	c.Echo().Logger.Infof("query = %q", query)
