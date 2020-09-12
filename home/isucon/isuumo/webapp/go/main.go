c96dce78e92c962fa320176bce752083529d5087
fono09 <fono@fono.jp>
2020-09-12 20:40:43 +0900

    些細なバグ修正

--- home/isucon/isuumo/webapp/go/main.go
+++ home/isucon/isuumo/webapp/go/main.go
@@ -377,7 +377,16 @@ func postEstate(c echo.Context) error {
 			return c.NoContent(http.StatusInternalServerError)
 		}
 
-		_, err = tx.Exec("INSERT INTO door_geom(id, g) VALUES(?,POINT(?,?))", id, minInt(doorHeight, doorWidth, doorWidth), maxInt(doorHeight, doorWidth, doorWidth))
+		var min, max int
+		if doorHeight < doorWidth {
+			min = doorHeight
+			max = doorWidth
+		} else {
+			max = doorHeight
+			min = doorWidth
+		}
+
+		_, err = tx.Exec("INSERT INTO door_geom(id, g) VALUES(?,POINT(?,?))", id, min, max)
 		if err != nil {
 			c.Logger().Errorf("failed to insert estate: %v", err)
 			return c.NoContent(http.StatusInternalServerError)
