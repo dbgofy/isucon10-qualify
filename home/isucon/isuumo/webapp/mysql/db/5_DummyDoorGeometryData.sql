7e43cd5fb204fe1e86b0e6a1e7902201c37997f0
fono09 <fono@fono.jp>
2020-09-12 18:42:02 +0900

    ミス修正

--- home/isucon/isuumo/webapp/mysql/db/5_DummyDoorGeometryData.sql
+++ home/isucon/isuumo/webapp/mysql/db/5_DummyDoorGeometryData.sql
@@ -1,1 +1,1 @@
-INSERT INTO isuumo.door_geom SELECT id, POINT(MIN(door_height, door_width), MAX(door_height, door_width)) FROM isuumo.estate;
+INSERT INTO isuumo.door_geom SELECT id, POINT(LEAST(door_height, door_width), GREATEST(door_height, door_width)) FROM isuumo.estate;
