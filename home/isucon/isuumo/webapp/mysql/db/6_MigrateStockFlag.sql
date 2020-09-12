eede2431215103b5bc6192955ea182b12cc44cb5
Goryudyuma(Kei.Matsumoto) <6211370+Goryudyuma@users.noreply.github.com>
2020-09-12 18:58:43 +0900

    case when

--- home/isucon/isuumo/webapp/mysql/db/6_MigrateStockFlag.sql
+++ home/isucon/isuumo/webapp/mysql/db/6_MigrateStockFlag.sql
@@ -1,3 +1,3 @@
 use isuumo
 UPDATE chair SET stock_flag = stock > 0;
-UPDATE estate SET rent_category = IF rent <= 50000 THEN 0 ELSEIF rent <= 100000 THEN 1 ELSEIF rent <= 150000 THEN 2 ELSE 3;
+UPDATE estate SET rent_category = (CASE WHEN rent <= 50000 THEN 0 WHEN rent <= 100000 THEN 1 WHEN rent <= 150000 THEN 2 ELSE 3 END);
