474315f47268476bc68d62ad9801768692cd6ce8
bgpat <bgpat23@gmail.com>
2020-09-12 13:43:55 +0900

    Fix

--- home/isucon/isuumo/webapp/go/newrelic.go
+++ home/isucon/isuumo/webapp/go/newrelic.go
@@ -9,31 +9,33 @@ import (
 
 const NEWRELIC_TXN = "newrelic-txn"
 
-func newrelicMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigAppName("isucon10q"),
-		newrelic.ConfigLicense(os.Getenv("NEWRELIC_LICENSE_KEY")),
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	return func(c echo.Context) error {
-		txn := app.StartTransaction(c.Request().Method + " " + c.Path())
-		defer txn.End()
-
-		txn.SetWebResponse(c.Response().Writer)
-		r := c.Request()
-		txn.SetWebRequestHTTP(r)
-
-		r = newrelic.RequestWithTransactionContext(r, txn)
-
-		c.Set(NEWRELIC_TXN, txn)
-
-		err := next(c)
+func newrelicMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		app, err := newrelic.NewApplication(
+			newrelic.ConfigAppName("isucon10q"),
+			newrelic.ConfigLicense(os.Getenv("NEWRELIC_LICENSE_KEY")),
+		)
 		if err != nil {
-			txn.NoticeError(err)
+			panic(err)
 		}
-		return err
+
+		return func(c echo.Context) error {
+			txn := app.StartTransaction(c.Request().Method + " " + c.Path())
+			defer txn.End()
+
+			txn.SetWebResponse(c.Response().Writer)
+			r := c.Request()
+			txn.SetWebRequestHTTP(r)
+
+			r = newrelic.RequestWithTransactionContext(r, txn)
+
+			c.Set(NEWRELIC_TXN, txn)
+
+			err := next(c)
+			if err != nil {
+				txn.NoticeError(err)
+			}
+			return err
+		}
 	}
 }
