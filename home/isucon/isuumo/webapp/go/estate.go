620f878272e5cd29d28481eb7a19e22720d0b064
bgpat <bgpat23@gmail.com>
2020-09-12 16:11:04 +0900

    Fix table name

--- home/isucon/isuumo/webapp/go/estate.go
+++ home/isucon/isuumo/webapp/go/estate.go
@@ -7,7 +7,7 @@ import (
 func listEstatesInPolygon(c Coordinates) ([]Estate, error) {
 	estates := []Estate{}
 	query := fmt.Sprintf(
-		`SELECT estate.* FROM estate JOIN estate_location ON estate.id = estate_location.id WHERE ST_Contains(ST_PolygonFromText(%s), estate_location.location) ORDER BY state.popularity DESC, estate.id ASC`,
+		`SELECT estate.* FROM estate JOIN estate_location ON estate.id = estate_location.id WHERE ST_Contains(ST_PolygonFromText(%s), estate_location.location) ORDER BY estate.popularity DESC, estate.id ASC`,
 		c.coordinatesToText(),
 	)
 	err := db.Select(&estates, query)
