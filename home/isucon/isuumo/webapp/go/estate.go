421eaf8a41406c6d7b6b90fda214efe086681a5f
Goryudyuma(Kei.Matsumoto) <6211370+Goryudyuma@users.noreply.github.com>
2020-09-12 19:14:46 +0900

    estateを忘れていた

--- home/isucon/isuumo/webapp/go/estate.go
+++ home/isucon/isuumo/webapp/go/estate.go
@@ -9,7 +9,7 @@ import (
 func listEstatesInPolygon(c echo.Context, coordinates Coordinates) ([]Estate, error) {
 	estates := []Estate{}
 	query := fmt.Sprintf(
-		`SELECT estate.* FROM estate JOIN estate_location ON estate.id = estate_location.id WHERE ST_Contains(ST_PolygonFromText(%s), estate_location.location) ORDER BY estate.popularity DESC, estate.id ASC`,
+		`SELECT estate.id, estate.thumbnail, estate.name, estate.description, estate.latitude, estate.longitude, estate.address, estate.rent, estate.door_height, estate.door_width, estate.features, estate.popularity FROM estate JOIN estate_location ON estate.id = estate_location.id WHERE ST_Contains(ST_PolygonFromText(%s), estate_location.location) ORDER BY estate.popularity DESC, estate.id ASC`,
 		coordinates.coordinatesToText2(),
 	)
 	c.Echo().Logger.Infof("query = %q", query)
