82bc4de7fc10c092cca1f009d6f4be38ee0c3bbb
fono09 <fono@fono.jp>
2020-09-12 20:00:49 +0900

    Areaである必要がない

--- home/isucon/isuumo/webapp/mysql/db/6_MigrateStockFlag.sql
+++ home/isucon/isuumo/webapp/mysql/db/6_MigrateStockFlag.sql
@@ -4,15 +4,13 @@ UPDATE chair SET stock_flag = stock > 0;
 INSERT INTO chair_geom 
   SELECT 
     id, 
-    ST_AREA(
-      ST_GEOMFROMTEXT(
-        CONCAT('POLYGON ((',
-                LEAST(height, width, depth), ' ', (height + width + depth - LEAST(height, width, depth) - GREATEST(height, width, depth)), ', ',
-                LEAST(height, width, depth), ' 1000, '
-                '1000 1000, ',
-                '1000 ', (height + width + depth - LEAST(height, width, depth) - GREATEST(height, width, depth)), ', ',
-                LEAST(height, width, depth), ' ', (height + width + depth - LEAST(height, width, depth) - GREATEST(height, width, depth)), '))'
-        )
+    ST_GEOMFROMTEXT(
+      CONCAT('POLYGON ((',
+              LEAST(height, width, depth), ' ', (height + width + depth - LEAST(height, width, depth) - GREATEST(height, width, depth)), ', ',
+              LEAST(height, width, depth), ' 1000, '
+              '1000 1000, ',
+              '1000 ', (height + width + depth - LEAST(height, width, depth) - GREATEST(height, width, depth)), ', ',
+              LEAST(height, width, depth), ' ', (height + width + depth - LEAST(height, width, depth) - GREATEST(height, width, depth)), '))'
       )
     )
     FROM chair;
